0793391797a69ca5b17857f69bf7f076a1abde01
Atheriel <atheriel@gmail.com>
2014-01-06 22:36:38 -0500

    Turns out the headers are necessary after all...
    Signed-off-by: Atheriel <atheriel@gmail.com>

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, Extension
+from platform import mac_ver
+
+# Deal with the new location of headers in Mavericks
+if mac_ver()[0].startswith('10.9'):
+    header_dir = '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/HIServices.framework/Versions/A/Headers'  # noqa
+else:
+    header_dir = '/System/Library/Frameworks/ApplicationServices.framework/Frameworks/HIServices.framework/Headers'
 
 setup(
     name = 'accessibility',
@@ -23,6 +30,7 @@ setup(
 
     ext_modules = [Extension('accessibility',
         sources = ['accessibility.c'],
+        include_dirs = [header_dir],
         # Uncomment the next line to include debug symbols while compiling
         # extra_compile_args = ['-g'],
         extra_link_args = ['-framework', 'ApplicationServices', '-v']
