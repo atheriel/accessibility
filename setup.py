60f18398152e25efbb9527b0eb4b36ec9f7f2323
xinhuang <xinhuang.abc@gmail.com>
2019-01-22 21:22:23 -0500

    Add support for macOS 10.12

--- setup.py
+++ setup.py
@@ -4,20 +4,22 @@ from platform import mac_ver
 # Deal with the new location of headers in Mavericks
 if mac_ver()[0].startswith('10.9'):
     header_dir = '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/HIServices.framework/Versions/A/Headers'  # noqa
+elif mac_ver()[0].startswith('10.12'):
+    header_dir = '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.12.sdk/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/HIServices.framework/Versions/A/Headers'  # noqa
 else:
     header_dir = '/System/Library/Frameworks/ApplicationServices.framework/Frameworks/HIServices.framework/Headers'
 
 setup(
-    name = 'accessibility',
-    description = 'Extension module that wraps the Accessibility API for Mac OS X.',
-    long_description = open('README.rst', 'r').read(),
-    version = '0.4.0',
-    author = 'Aaron Jacobs',
-    author_email = 'atheriel@gmail.com',
-    url = 'https://github.com/atheriel/accessibility',
-    license = 'ISC License',
-    platforms = ['MacOS X'],
-    classifiers = [
+    name='accessibility',
+    description='Extension module that wraps the Accessibility API for Mac OS X.',
+    long_description=open('README.rst', 'r').read(),
+    version='0.4.0',
+    author='Aaron Jacobs',
+    author_email='atheriel@gmail.com',
+    url='https://github.com/atheriel/accessibility',
+    license='ISC License',
+    platforms=['MacOS X'],
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
@@ -30,16 +32,17 @@ setup(
         'Topic :: Utilities'
     ],
 
-    ext_modules = [Extension('accessibility',
-        sources = ['accessibility.c'],
-        include_dirs = [header_dir],
-        # Uncomment the next line to include debug symbols while compiling
-        # extra_compile_args = ['-g'],
-        extra_compile_args = ['-Wno-error=unused-command-line-argument-hard-error-in-future'],
-        extra_link_args = ['-framework', 'ApplicationServices', '-v']
-    )],
+    ext_modules=[Extension('accessibility',
+                           sources=['accessibility.c'],
+                           include_dirs=[header_dir],
+                           # Uncomment the next line to include debug symbols while compiling
+                           # extra_compile_args = ['-g'],
+                           extra_compile_args=[
+                               '-Wno-error=unused-command-line-argument-hard-error-in-future'],
+                           extra_link_args=['-framework', 'ApplicationServices', '-v']
+                           )],
 
-    install_requires = [
+    install_requires=[
         # 'docutils>=0.3',
     ]
 )
